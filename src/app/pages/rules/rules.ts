bb25b14a3283cb2bec19601881442ed759d6c83d
Seth <sethgoldin27@gmail.com>
2020-06-04 16:06:48 -0400

    gitignore, rulesPulling in RuleService, content updating in rules.ts

--- src/app/pages/rules/rules.ts
+++ src/app/pages/rules/rules.ts
@@ -18,10 +18,18 @@ export class RulesPage {
     ) {
         this.searching = false;
         this.searchReference = {};
+        this.hideBack = true;
+        this.array = this.rules.mainRules;
     }
     public searchTerm: string;
     public searching;
     public searchReference;
+    hideBack: boolean;
+    sectionrule: any;
+    currentTerm: string;
+    subsection: string[];
+    section: string[];
+    array: string[];
 
     // when searchbar changes, update array. when searchbar is empty, display main rules categories
     searchChanged() {
@@ -29,18 +37,49 @@ export class RulesPage {
         this.searchTerm = this.searchTerm.toLowerCase();
 
         if (this.searching) {
-            this.rules.getAllRulesMatching(this.searchTerm);
+            this.hideBack = true;
+            this.array = this.rules.getAllRulesMatching(this.searchTerm);
         } else {
-            this.rules.array = this.rules.mainRules;
+            this.array = this.rules.mainRules;
         }
     }
 
-    // determine if a given rule is numeric or a glossary term
-    isRule(ruleToCheck) {
-        return this.isNumeric(ruleToCheck.substring(0, 2));
+    // when a rule is clicked, displays the subcategories or detailed rules that category contains
+    displaySubrules(rule) {
+        // if the main rules are currently displayed (default), and a rule is clicked, display all rules its "section" represents, show back button
+        this.hideBack = false;
+        if (this.array == this.rules.mainRules) {
+            this.array = this.rules.getRuleSecById(rule);
+            // sets the back-button label
+            this.currentTerm = rule;
+            // when a section is displayed or searched, and a rule is clicked, display all rules its "subsection" represents
+        } else if (this.searchReference != {}) {
+            // set sectionrule to glossary by default
+            this.sectionrule = this.rules.glossary;
+            // if not glossary, set sectionrule to appropriate section
+            for (let i = 0; i < this.rules.mainRules.length; i++) {
+                if (this.rules.mainRules[i].startsWith(rule.substring(0, 1))) {
+                    this.sectionrule = this.rules.mainRules[i];
+                }
+            }
+            this.array = this.rules.getRuleSubSecById(this.sectionrule, rule);
+            this.subsection = this.array;
+            this.currentTerm = rule;
+        }
     }
 
-    isNumeric(value) {
-        return /^-{0,1}\d+$/.test(value);
+    // resets the array to default main rules categories, hides back button
+    returnMainRules() {
+        this.array = this.rules.mainRules;
+        this.hideBack = true;
+    }
+
+    // determines whether the array is currently displaying "subsection" rules or not
+    isSubsecRule() {
+        if (this.array == this.subsection) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
